7fff3f62429acbfcc3c8747e1b32d6088f9f540f
iamananth <ananthrshenoy@gmail.com>
2022-11-14 19:42:08 +0530

    comments

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ import express from "express";
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 },{
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 },{
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -19,7 +22,21 @@ app.put('/api/articles/:name/upvote', (req,res) => {
     const article = articlesInfo.find(a => a.name === name);
     if(article){
         article.upvotes += 1;
-        res.send(`The ${name} articles now has ${article.upvotes} upvotes!!`);
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!!`);
+    }else{
+        res.send('That Article doesn\'t exist');
+    }
+    
+});
+
+app.post('/api/articles/:name/comments', (req,res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+    if(article){
+        article.comments.push({ postedBy, text});
+        res.send(article.comments);
     }else{
         res.send('That Article doesn\'t exist');
     }
